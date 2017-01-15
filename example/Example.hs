32270c26f9d3318e6faaf2b0e1be50f7630915b7
Samuel Schlesinger <sgschlesinger@gmail.com>
2017-01-15 16:20:27 -0500

    merged fixes with master

--- example/Example.hs
+++ example/Example.hs
@@ -2,7 +2,4 @@ import Graphics.Gross
 import UI.NCurses
 import Control.Monad.Trans
 
-main = runCurses $ do
-  display $ \x y -> if (x `mod` y) `mod` 4 == 1 then '@' else ' '
-  render
-  liftIO getChar
+main = pure ()
